b0016d7ff32743ba3d1099a1dbeffb01f070d660
Adrienne Paquin <adriennepaquin@gmail.com>
2021-03-26 18:25:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyCustomHolidayTo(name) {
+function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
